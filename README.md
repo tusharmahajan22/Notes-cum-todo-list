96457fc7a55ce82a5e34ea957e11e01d2df90536
Tushar Mahajan <tusharmahajan1114@gmail.com>
2023-08-24 13:15:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Keeper-App
+
+Deployed Site - https://keeper-app-nitish.netlify.app/
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
+
+function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  function expanded() {
+    setExpanded(true);
+  }
+  return (
+    <div>
+      <form className="create-note">
+        {isExpanded ? (
+          <input
+            onChange={handleChange}
+            value={note.title}
+            name="title"
+            placeholder="Title"
+          />
+        ) : null}
+        <textarea
+          onClick={expanded}
+          onChange={handleChange}
+          value={note.content}
+          name="content"
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+        />
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
